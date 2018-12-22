35c27c4624537824cb9450a9096c3f0ead0ede0a
Leha <avoroncovs@gmail.com>
2018-12-22 22:21:31 +0200

    implemented currency code validator

---
+++ src/main/java/me/bill/exception/CurrencyCodeValidationException.java
@@ -0,0 +1,8 @@
+package me.bill.exception;
+
+public class CurrencyCodeValidationException extends Exception {
+
+    public CurrencyCodeValidationException(String message) {
+        super(message);
+    }
+}
